3978fe4a788bfb76f5b562c6480b0c0e086bb1a8
Gaurav-Jang <gauravjangra936@gmail.com>
2024-06-29 21:57:57 +0530

    Fees Payment Ui --- half done

--- src/components/Pages/FeesPayment/FeesPayment.jsx
+++ src/components/Pages/FeesPayment/FeesPayment.jsx
@@ -20,101 +20,106 @@ const FeesPayment = () => {
       >
         <div className="container">
           {/* search box */}
-          <div className="text-center">Search</div>
+          <div>
+            <input className="form-control" type="search" name="search" />
+            <button className="btn btn-primary">Search</button>
+          </div>
 
-          {/* data fields */}
-          <div className="flex mt-10">
-            {/* left */}
-            <div className="w-100">
-              <div className="col-md-6 space-y-5">
-                {/* student name */}
-                <div className="form-group">
-                  <label htmlFor="name">Student Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Student Name"
-                  />
+          <div className="feesPayment d-flex justify-content-between align-items-center">
+            {/* left section */}
+            <div className="left-section">
+              {/* 1 */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">StudentName</label>
+                  <span>:-</span>
                 </div>
-
-                {/* father's name */}
-                <div className="form-group">
-                  <label htmlFor="name">Father Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Father Name"
-                  />
+                <span className="data">Gaurav</span>
+              </div>
+              {/* 2 */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">Date of birth</label>
+                  <span>:-</span>
                 </div>
-
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Enter Name"
-                  />
+                <span className="data">02-02-2003</span>
+              </div>
+              {/* 3  */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">FatherName</label>
+                  <span>:-</span>
                 </div>
-
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Enter Name"
-                  />
+                <span className="data">Dinesh Kumar</span>
+              </div>
+              {/* 4 */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">ClassRoom Enrolled</label>
+                  <span>:-</span>
                 </div>
+                <span className="data">CSE</span>
+              </div>
+              {/* 5 */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">Availing Hostel</label>
+                  <span>:-</span>
+                </div>
+                <span className="data">No</span>
+              </div>
+              {/* 6 */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">Availing Transport</label>
+                  <span>:-</span>
+                </div>
+                <span className="data">No</span>
+              </div>
+              {/* 7 */}
+              <div className="left-strip">
+                <div className="set-label">
+                  <label className="form-label">Availing School</label>
+                  <span>:-</span>
+                </div>
+                <span className="data">Yes</span>
               </div>
             </div>
 
-            {/* right */}
-            <div className="w-100">
-              <div className="col-md-6 space-y-5">
-                {/* student name */}
-                <div className="form-group">
-                  <label htmlFor="name">Student Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Student Name"
-                  />
-                </div>
-
-                {/* father's name */}
-                <div className="form-group">
-                  <label htmlFor="name">Father Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Father Name"
-                  />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Enter Name"
-                  />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Enter Name"
-                  />
-                </div>
+            {/* right section */}
+            <div className="right-section">
+              {/* 1 */}
+              <div className="input-strip">
+                <label className="form-label">Course fees</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  placeholder="Enter course fees"
+                />
+              </div>
+              <div className="input-strip">
+                <label className="form-label">Transport fees</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  placeholder="Enter transport fees"
+                />
+              </div>
+              <div className="input-strip">
+                <label className="form-label">School fees</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  placeholder="Enter School fees"
+                />
+              </div>
+              <div className="input-strip">
+                <label className="form-label">Hostel fees</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  placeholder="Enter hostel fees"
+                />
               </div>
             </div>
           </div>
